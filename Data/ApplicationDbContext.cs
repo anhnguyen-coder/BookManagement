d2a05db3777f7ccb6ead5e305d4ad8543bbb5b08
Nguyen Duc Anh <128030928+anhnguyen-coder@users.noreply.github.com>
2023-10-22 21:27:04 +0700

    Add permission for logging and Create BookController and Book views
    Add permission for logging and Create BookController and Book views

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BookManagement.Models;
+
+namespace BookManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BookManagement.Models.Book>? Book { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookManagement.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price {  get; set; }
+        public string UrlImage {  get; set; }
+        public int Quantity {  get; set; }
+        public string Category { get; set; }
+    }
+}
